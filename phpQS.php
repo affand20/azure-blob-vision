c4a05a37dd81b91269df1124db869767e31607ff
affand20 <fahri.lafa@gmail.com>
2019-05-04 14:43:40 +0700

    insyaa allah fix

--- phpQS.php
+++ phpQS.php
@@ -163,7 +163,7 @@ else
 <html>
 <head>
     <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
 </head>
 <body>
  
